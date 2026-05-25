4c5d38a6b731a8aee4cd498141e5311e3dcdac4d
Syed <5y3d@example.com>
2026-05-25 22:52:21 +0100

    now send our normalised data to the backend for processing. Co-authored-by: jonnorrie <jonathontirabassi@gmail.com> Co-authored-by: lewisian8787 <lewisian8787@gmail.com> Co-authored-by: Nazeer Shaikh <63686800+nash226@users.noreply.github.com>

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -13,13 +13,30 @@ interface Recommendation {
   explanation: string;
 }
 
-interface RecommendationProps {
+interface RecommendationItemProps {
   rec: Recommendation,
   handleAccept: (rec: Recommendation) => void,
   handleDecline: (rec: Recommendation) => void;
 }
 
-function RecommendationItem({ rec, handleAccept, handleDecline }: RecommendationProps) {
+interface AcceptedLabels {
+  [key: string]: {
+    problemLabels: string[];
+    allLabels: string[];
+  }
+}
+
+function RecommendationItem({ rec, handleAccept, handleDecline }: RecommendationItemProps) {
+
+  const executeHandleAccept = (event: React.SyntheticEvent): void => {
+    event.preventDefault();
+    handleAccept(rec);
+  }
+
+  const executeHandleDecline = (event: React.SyntheticEvent): void => {
+    event.preventDefault();
+    handleDecline(rec);
+  }
   return (
     <li>
       <div>
@@ -31,17 +48,16 @@ function RecommendationItem({ rec, handleAccept, handleDecline }: Recommendation
       <div>
         Estimated reduction metric series: {rec.estimated_reduction_percent}%
       </div>
-      <button onClick={() => {handleAccept(rec)}}>Accept</button>
-      <button onClick={() => {handleDecline(rec)}}>Decline</button>
+      <button onClick={executeHandleAccept}>Accept</button>
+      <button onClick={executeHandleDecline}>Decline</button>
     </li>
   )
 }
 
-type Decision = 'accepted' | 'declined' | 'pending';
+// type Decision = 'accepted' | 'declined' | 'pending';
 
 function App(_props: AppRootProps) {
   const [recs, setRecs] = useState<Recommendation[]>([]);
-
   const handleAccept = (rec: Recommendation) => {
     setRecs(prev => prev.map(currRec => currRec === rec ? {...rec, status: 'accepted' } : currRec));
   };
@@ -49,6 +65,29 @@ function App(_props: AppRootProps) {
   const handleDecline = (rec: Recommendation) => {
     setRecs(prev => prev.map(currRec => currRec === rec ? {...rec, status: 'declined' } : currRec));
   };
+
+  const handleSubmit = async (event: React.SyntheticEvent) => {
+    event?.preventDefault();
+    if (recs.some(rec => rec.status === "pending")) {
+      alert("you have some pending decisions; please accept or reject all objects.");
+      return;
+    }
+    const output: AcceptedLabels = {};
+    recs.forEach(rec => {
+      if (rec.status === "accepted") {
+        if (rec.metric_name in output) {
+          output[rec.metric_name].problemLabels.push(rec.problem_label);
+        } else {
+          output[rec.metric_name] = {
+            problemLabels: [rec.problem_label],
+            allLabels: [...rec.remaining_labels, rec.problem_label]
+          }
+        }
+      }
+    })
+    console.log(output);
+    await axios.post("http://localhost:3001/api/acceptedRecommendations", output);
+  }
 
   useEffect(() => {
     console.log(recs);
@@ -68,15 +107,16 @@ function App(_props: AppRootProps) {
   }, []);
 
   return (
-    <div>
-      <h1>recommendations:</h1>
+    <form onSubmit={handleSubmit}>
+      <h3>recommendations:</h3>
       {recs.map(rec => <RecommendationItem 
-      key={rec.metric_name + ' ' + rec.problem_label} 
-      rec={rec}
-      handleAccept={handleAccept}
-      handleDecline={handleDecline}
-    />)}
-    </div>
+        key={rec.metric_name + ' ' + rec.problem_label} 
+        rec={rec}
+        handleAccept={handleAccept}
+        handleDecline={handleDecline}
+      />)}
+      <button type='submit'>Submit</button>
+    </form>
   );
 }
 
